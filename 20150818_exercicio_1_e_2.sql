e1ef0e8af9f656dfbba355b7bc8121936ac68664
Diogo_DC <diogodinizcosme@gmail.com>
2017-11-20 20:06:38 -0200

    Update 20150818_exercicio_1_e_2.sql

--- 20150818_exercicio_1_e_2.sql
+++ 20150818_exercicio_1_e_2.sql
@@ -4,42 +4,42 @@ go
 -- Criar uma tabela no DB Northwind chamada HistoricoVendas (DescricaoProduto,
 -- ValorMedioVenda, qtdItensVendidos, NomeCategoria, NomeFornecedor)
 
---create table HistoricoVendas (
---	DescricaoProduto varchar(50),
---	ValorMedioVenda money,
---	qtdItensVendidos integer,
---	NomeCategoria varchar(50),
---);
+create table HistoricoVendas (
+	DescricaoProduto varchar(50),
+	ValorMedioVenda money,
+	qtdItensVendidos integer,
+	NomeCategoria varchar(50),
+);
 
 
 
 
--- Criar um SP chamado SPAlimentaHistorico que recebe como parâmetro uma
--- data inicial e uma data final de pedidos. Caso o valor médio de cada item
--- vendido neste período seja superior a 30 reais, inserir esta venda na tabela
--- HistoricoVendas
+-- Criar um SP chamado SPAlimentaHistorico que recebe como parÃ¢metro uma
+-- data inicial e uma data final de pedidos. Caso o valor mÃ©dio de cada item
+-- vendido neste perÃ­odo seja superior a 30 reais, inserir esta venda na tabela
+ --HistoricoVendas.
 
---create proc SPAlimentaHistorico(@dataInicial date, @dataFinal date) as
---insert into HistoricoVendas 
---	select	P.ProductName,
---			avg(OD.Quantity * OD.UnitPrice),
---			sum(OD.Quantity),
---			CA.CategoryName
+create proc SPAlimentaHistorico(@dataInicial date, @dataFinal date) as
+insert into HistoricoVendas 
+	select	P.ProductName,
+			avg(OD.Quantity * OD.UnitPrice),
+			sum(OD.Quantity),
+			CA.CategoryName
 
---	from Orders O	inner join [Order Details] OD on O.OrderID = OD.OrderID
---					inner join Products P on OD.ProductID = P.ProductID
---					inner join Categories CA on P.CategoryID = CA.CategoryID
+	from Orders O	inner join [Order Details] OD on O.OrderID = OD.OrderID
+					inner join Products P on OD.ProductID = P.ProductID
+					inner join Categories CA on P.CategoryID = CA.CategoryID
 
---	where O.OrderDate between @dataInicial and @dataFinal
+	where O.OrderDate between @dataInicial and @dataFinal
 
---	group by	P.ProductName,
---				CA.CategoryName
+	group by	P.ProductName,
+				CA.CategoryName
 
---	having avg(OD.Quantity * OD.UnitPrice) > 30;
-
-
+having avg(OD.Quantity * OD.UnitPrice) > 30;
 
 
--- exec SPAlimentaHistorico '01-01-1996', '12-31-1998'
+
+
+exec SPAlimentaHistorico '01-01-1996', '12-31-1998'
 
 
